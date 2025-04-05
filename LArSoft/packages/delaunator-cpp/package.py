b8421220e16296cf658f0118c58319b7b0ff3f76
Nathaniel Rowe <nathanielrowe2001@gmail.com>
2025-04-05 13:43:07 +0000

    LArSoft versioning style changes

--- LArSoft/packages/delaunator-cpp/package.py
+++ LArSoft/packages/delaunator-cpp/package.py
@@ -8,8 +8,9 @@ from spack.package import *
 class DelaunatorCpp(Package):
     """FIXME: Put a proper description of your package here."""
 
-    url = "https://github.com/delfrrr/delaunator-cpp/archive/refs/tags/v0.4.0.tar.gz"
+    url = "https://github.com/abellgithub/delaunator-cpp/archive/refs/tags/v0.4.0.tar.gz"
 
+    version("1.0.0", sha256="27fdcbd92b3d0147a3952ee61fb34e86cfc3e87b7fb05afccca045582d0a83f4")
     version("0.4.0", sha256="8a885c5984dd586f6658276594d0a601ebe2612fe66e62fda2d19d855faddc0b")
     version("0.3.0", sha256="a796688322866502ed26a60f6bb59e2c602d13ac28d251ebb1f16902dba30471")
     version("0.2.0", sha256="e0a583c10a6a97db1153c1b37b79f05291c1b7c1d37e7d9063c161c21494b0e1")
--- LArSoft/packages/larfinder/package.py
+++ LArSoft/packages/larfinder/package.py
@@ -5,17 +5,18 @@
 
 
 from spack.package import *
+from spack.pkg.fnal_art.fnal_github_package import *
 
 
-class Larfinder(CMakePackage):
+class Larfinder(CMakePackage, FnalGithubPackage):
     """Common cmake bits for larsoft"""
 
-    homepage = "https://github.com/LArSoft"
-    url = "https://github.com/LArSoft/larfinder/archive/tags/v09_00_01.tar.gz"
-
+    repo = "LArSoft/larfinder"
+    version_patterns = ["v09_00_01"]
     maintainers = ["marcmengel"]
 
-    version("09_00_01", sha256="cfd2ce200a032e2e7cc7e8c510d2719965fb17e5571132e18be8574ad8a20144")
+    version("09.00.02", sha256="6eaf11a9625832a0e301d46354c06ac420eb4e3a88f26d5d9603eccd1238c380")
+    version("09.00.01", sha256="5edbc7eb8a8aa17b4524b72cc1f8ab03691ea730511db0c6167731a9c2e1d659")
     version("develop", branch="develop", get_full_repo=True)
 
     depends_on("cetmodules", type="build")
